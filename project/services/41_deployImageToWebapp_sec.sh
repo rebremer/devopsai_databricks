69e2d6ee1fccf7ac8441881b06802d98f407b5fa
Rene Bremer <rebremer@microsoft.com>
2019-03-02 22:38:03 +0100

    adjusted release to web app script

--- project/services/41_deployImageToWebapp_sec.sh
+++ project/services/41_deployImageToWebapp_sec.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #variables
-webapp=$1
-registry=$2
-resource_grp=$3
-# $(echo $(az acr list --resource-group $(rg-name) --query [0].name)  | tr -d '"')
 image=$(echo $(cat conf/image.json) | jq -r '.image_name'):$(echo $(cat conf/image.json) | jq -r '.image_version')
-password=$(echo $(az acr credential show -n ${registry} --query passwords[0].value)  | tr -d '"')
+password=$(echo $(az acr credential show -n $2 --query passwords[0].value)  | tr -d '"')
 #update webapp with container
-az webapp config container set --name ${webapp} --resource-group ${resource_grp} --docker-custom-image-name ${registry}.azurecr.io/$image --docker-registry-server-url https://${registry}.azurecr.io --docker-registry-server-user ${registry} --docker-registry-server-password $password
+az webapp config container set --name $1 --resource-group $3 --docker-custom-image-name ${2}.azurecr.io/$image --docker-registry-server-url https://${2}.azurecr.io --docker-registry-server-user ${2} --docker-registry-server-password $password
