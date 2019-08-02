fd91572e4b209b797e995fc7e5e2ea38611008e3
hfriedrich <heiko.friedrich@researchstudio.at>
2019-08-02 15:38:06 +0200

    add tags from model to image sec

--- project/services/30_createImageFromModel_sec.py
+++ project/services/30_createImageFromModel_sec.py
@@ -42,7 +42,7 @@ ws = Workspace(workspace_name = workspace,
 
 model_list = Model.list(workspace=ws)
 model, = (m for m in model_list if m.version == model_version and m.name == model_name)
-print("Model picked: {} \nModel Description: {} \nModel Version: {}".format(model.name, model.description, model.version))
+print("Model picked: {} \nModel Description: {} \nModel Version: {} \nModel Tags: {}".format(model.name, model.description, model.version, model.tags))
 
 os.chdir("deploy")
 
@@ -66,6 +66,7 @@ image = Image.create(name = service_name,
                      workspace = ws)
 
 image.wait_for_creation(show_output = True)
+image.add_tags(model.tags)
 
 # Writing the model and image details to /aml_config/image.json
 
