8615e4e2401927dc2f2b73f1cc11e0148dd1bacc
hfriedrich <heiko.friedrich@researchstudio.at>
2019-08-02 15:44:38 +0200

    update azureml-sdk[cli] to version 1.0.53

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python --version
 #pip install azure-cli==2.0.46
-pip install --upgrade azureml-sdk[cli]==0.1.80
+pip install --upgrade azureml-sdk[cli]==1.0.53
 pip install azure-cli-core
 pip install numpy
